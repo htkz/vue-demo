27614297249eeb85650f734c8979580fb59d2c75
htkz <htkzmo@gmail.com>
2018-02-12 22:36:19 +0800

    Config for display

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './vue-demo/resume-maker/dist',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
