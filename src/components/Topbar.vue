f2fedf5bc8922ecfb4c068e0a00f233559caeda4
htkz <htkzmo@gmail.com>
2018-02-12 22:32:14 +0800

    Added style

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,6 +1,8 @@
 <template lang="html">
   <div id="topbar">
-    I am topbar
+    <div class="wrapper">
+      <span class="logo">Resumer</span>
+    </div>
   </div>
 </template>
 
@@ -12,6 +14,26 @@ export default {
 
 <style lang="css">
   #topbar {
-    color: green;
+    background: #fff;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .25);
+    height: 64px;
+  }
+
+  #topbar > .wrapper {
+    min-width: 1024px;
+    max-width: 1440px;
+    margin: 0 auto;
+    height:64px;
+  }
+
+  #topbar > .wrapper {
+    display: flex;
+    align-items: center;
+    padding: 0 16px;
+  }
+
+  #topbar .logo {
+    font-size: 24px;
+    color: black;
   }
 </style>
